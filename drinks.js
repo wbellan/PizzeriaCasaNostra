279303f6bcf7f1b2af030f14cdf75ae4033188b9
Sarah Micheletti <sarah.c.micheletti@gmail.com>
2018-11-05 21:41:52 -0600

    Lots of changes. Updated the navigation to include dropdown for the Menu section as well as Order Online. Created a drinks page which required some CSS updates and minor javascript updates for formatting the Beer section. Applied similar updates in the CSS and formatting for the Additional Toppings section on the food menu.

---
+++ drinks.js
@@ -0,0 +1,171 @@
+var menu = [{
+    "name": "Whites",
+    "description": null,
+    "items": [{
+        "name": "Damilano",
+        "description": "Arneis 2016",
+        "register": "Langhe, Piemonte"
+      },
+      {
+        "name": "Long Barn",
+        "description": "Chardonnay 2016",
+        "register": "California"
+      },
+      {
+        "name": "Michel Schlumberger",
+        "description": "Chardonnay XXXX",
+        "register": "California"
+      },
+      {
+        "name": "Hess Collection",
+        "description": "Chardonnay 2016",
+        "register": "Napa Valley"
+      },
+      {
+        "name": "Hess Collection",
+        "description": "Chardonnay",
+        "register": "California (On Tap)"
+      },
+      {
+        "name": "Freemark Abbey Winery",
+        "description": "Chardonnay XXXX",
+        "register": "Napa Valley"
+      },
+      {
+        "name": "Feudi di San Gregorio",
+        "description": "Falanghina del Sannio XXXX",
+        "register": "Campania, Italy"
+      },
+      {
+        "name": "Sant'Anna",
+        "description": "Pinot Grigio 2016",
+        "register": "Veneto, Italy"
+      },
+      {
+        "name": "99 West",
+        "description": "Pinot Gris XXXX",
+        "register": "Willamette, Oregon"
+      },
+      {
+        "name": "Elk Cove Vineyards",
+        "description": "Pinot Gris 2017",
+        "register": "Willamette, Oregon"
+      },
+      {
+        "name": "Kim Crawford",
+        "description": "Sauvignon Blanc XXXX",
+        "register": "New Zealand"
+      },
+      {
+        "name": "Montelvini Venegazzu",
+        "description": "Prosecco XXXX",
+        "register": "Veneto (On Tap)"
+      },
+    ]
+  },
+  {
+    "name": "Reds",
+    "description": null,
+    "items": [{
+        "name": "Brandini",
+        "description": "Barbera d'Alba XXXX",
+        "register": "Piemonte, Italy"
+      },
+      {
+       "name": "Chevriot Winery",
+        "description": "Cabernet Sauvignon XXXX",
+        "register": "California"
+      },
+      {
+       "name": "Cupcake Vineyards",
+        "description": "Cabernet Sauvignon",
+        "register": "California (On Tap)"
+      },
+      {
+       "name": "Michel Schlumberger",
+        "description": "Cabernet Sauvignon 2016",
+        "register": "California"
+      },
+      {
+       "name": "Tiziano",
+        "description": "Chianti 2015",
+        "register": "Tuscany"
+      },
+      {
+       "name": "Giacomo Borgogno",
+        "description": "Dolceto d'Alba 2014",
+        "register": "Piemonte, Italy"
+      },
+      {
+       "name": "Diseño",
+        "description": "Malbec Old Vine XXXX",
+        "register": "Argentina (On Tap)"
+      },
+      {
+       "name": "Doña Paula",
+        "description": "Malbec XXXX",
+        "register": "Valle de Uco, Argentina"
+      },
+      {
+       "name": "Graffingna Grand Reserva",
+        "description": "Malbec XXXX",
+        "register": "San Juan, Argentina"
+      },
+      {
+       "name": "Illuminati",
+        "description": "Montepulciano D'Abruzzo Reparosso 2015",
+        "register": "Abruzzo, Italy"
+      },
+      {
+       "name": "Cantine Colosi",
+        "description": "Nero d'Avola 2015",
+        "register": "Sicily"
+      },
+      {
+       "name": "Butera",
+        "description": "Nero d'Avola 2015",
+        "register": "Sicily"
+      },
+      {
+       "name": "Long Barn",
+        "description": "Pinot Noir Estate 2016",
+        "register": "California"
+      },
+      {
+       "name": "Elk Cove Vineyards",
+        "description": "Pinot Noir Estate 2015",
+        "register": "Willamette, Oregon"
+      },
+      {
+       "name": "Petra Zingari",
+        "description": "Super Tuscan XXXX",
+        "register": "Tuscany"
+      },
+      {
+       "name": "Marchesi Biscardo",
+        "description": "Valpolicella Ripaso 2014",
+        "register": "Veneto, Italy"
+      },
+      {
+       "name": "Avignonesi",
+        "description": "Vino Nobile di Montepulciano",
+        "register": "Tuscany"
+      },
+    ]
+  },
+  {
+    "name": "Beers",
+    "description": null,
+    "items": [{
+        "name": "On Tap",
+        "description": "512 IPA, Thirsty Goat, Zoe, Miller Lite, Peroni, Buckethead IPA, Dos Equis, Shiner Bock, Pacifico, Stash IPA, Electric Jellyfish Celis White, Big Bend Hefeweizen, Love Street, Fat Tire, Austin Eastcider Blood Orange Cider",
+        "register": null
+      },
+      {
+       "name": "Bottle",
+        "description": "Corona, Corona Premier, Michelob Ultra, Budwesier, Bud Light, Coors Light, Angry Orchard, Sam Adams, Firemans 4, Moretti, Stella Artois, Guinness, Heineken",
+        "register": null
+      },
+    ]
+  }
+]
